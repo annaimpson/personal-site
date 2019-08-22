8a5bdae59b0b119320c2c852d99a7186536a14ef
Anna  Impson <annaimpson@Annas-iMac.attlocal.net>
2019-08-21 20:39:29 -0400

    removed unused content and restructured folders

--- app/scripts/index.js
+++
@@ -1,9 +0,0 @@
-var Backbone = require('backbone');
-var $ = require('jquery');
-
-var Router = require('./router');
-
-var realRouter = new Router();
-$(function(){
-  Backbone.history.start();
-});
